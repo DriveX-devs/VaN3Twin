18cd9e8d8650f79aa9b622c34e77aa5f8ba31354
Diego Gasco <diego.gasco99@gmail.com>
2026-04-28 21:45:50 +0200

    Updated main file

--- src/automotive/examples/v2v-foresee-mcm-80211p.cc
+++ src/automotive/examples/v2v-foresee-mcm-80211p.cc
@@ -90,7 +90,7 @@ int main (int argc, char *argv[])
   bool sumo_gui = false;
   bool store_coordinations_in_csv = true;
   int seed = 42;
-  int threads = 20;
+  int threads = 7;
 
   // Set here the path to the SUMO XML files
   std::string sumo_folder = "src/automotive/examples/sumo_files_v2v_foresee/";
